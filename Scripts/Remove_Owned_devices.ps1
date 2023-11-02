eb0d15df56d53ca56b4ae88a42bd0b1c1834fac6
jcl45 <120643287+jcl45@users.noreply.github.com>
2023-11-02 17:47:36 +0000

    Up

--- Scripts/Remove_Owned_devices.ps1
+++ Scripts/Remove_Owned_devices.ps1
@@ -22,20 +22,35 @@ function CallMgGraph {
             }
 }
 
-#$UsrUPN = "james.cockerill@sky.uk"
+function Get-OwnDev {
+    param ($UsrID, $DFilter)
+    CallMgGraph -Meth 'Get' -Ver 'v1.0' -Res "users/$UsrID/ownedDevices"
+    $OwnDev = $GrRaw
+    if ($DFilter) {$OwnDev = $OwnDev | Where-Object operatingSystem -eq 'Windows'}
+    [System.Collections.ArrayList]$Script:AL = $OwnDev |
+        ForEach-Object {        
+            $_ | Select-Object `
+                @{n="Hostname";e={$_.displayName}},`
+                @{n="Make";e={$_.manufacturer}},`
+                @{n="Model";e={$_.model}},`
+                @{n="LastSync";e={$_.approximateLastSignInDateTime}},`
+                @{n="ID";e={$_.id}}`
+    }
+}
 
-
-
-#$DevSel = $AL | Out-GridView -Title "Select Devices to Remove" –PassThru
-if ($null -ne $DevSel) {
+function Remove-OwnDev {
+    param ($DevSel, $UsrID)
     $DevSel | ForEach-Object {
         CallMgGraph -Meth 'Delete' -Ver 'v1.0' -Res "users/$UsrID/ownedDevices/$($_.id)/$ref" #$ref is required https://learn.microsoft.com/en-us/graph/api/device-delete-registeredowners?view=graph-rest-1.0&tabs=http#http-request
     }
-}#>
+    #Get-OwnDev
+    #Search_Layout -DataSource $AL -Vis "Visible"
+}
+
 
 #WPF Window
 $CacheDir = "C:\ProgramData\Sky\Cache\ILA"
-<#if (!(Test-Path $CacheFldr)){Invoke-Expression -Command:"md $CacheFldr"}
+if (!(Test-Path $CacheFldr)){Invoke-Expression -Command:"md $CacheFldr"}
 Invoke-RestMethod -Uri "https://raw.githubusercontent.com/jcl45/CT_Dev/main/Master_GUI_v1.0/Master_GUI_v1.0/MainWindow.xaml" -OutFile "$CacheFldr\MainWindow.xaml"#>
 $inputXML = Get-Content "$CacheFldr\MainWindow.xaml" #XAML Raw
 $inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
@@ -63,74 +78,109 @@ $xaml.SelectNodes("//*[@Name]") | ForEach-Object {"trying item $($_.Name)";
 ## Main Window
 $MinHeight = 650 
 $MinWidth = 750
-$Form.MinHeight = $MinHeight
+#$Form.MinHeight = $MinHeight
 $Form.MinWidth = $MinWidth
 $Form.Title = "Manage Owned Devices"
 
 
 
 ## Child Elements
-$WPFRect1.Width = $MinWidth
-$WPFRect1.Height = 60
-$WPFRect1.Visibility = "Visible"
-$WPFTextB1.FontSize = 24
-$WPFTextB1.Text = "Enter UPN of User"
-$WPFTextB1.Visibility = "Visible"
-$WPFInpTxt1.HorizontalAlignment = "Left"
-$WPFInpTxt1.Width = 355
-$WPFInpTxt1.Height = 40
-$InpTxt1_Pos = 105
-$WPFInpTxt1.Margin = "$InpTxt1_Pos,78,0,0"
-$WPFInpTxt1.Visibility = "Visible"
-$WPFButton1.Content.Text = "OK"
-$WPFButton1.Content.FontSize = 20
-$WPFButton1.Width = 130
-$WPFButton1.Height = 40
-$WPFButton1.HorizontalAlignment = "Left"
-$WPFButton1.Margin = "$($InpTxt1_Pos +380),78,0,0"
-$WPFButton1.Visibility = "Visible"
+function Default_Layout {
+    $WPFRect1.Width = $MinWidth
+    $WPFRect1.Height = 60
+    $WPFRect1.Visibility = "Visible"
+    $WPFTextB1.Foreground = "#FFFFFFFF"
+    $WPFTextB1.FontSize = 24
+    $WPFTextB1.Text = "Enter UPN of User"
+    $WPFTextB1.Visibility = "Visible"
+    $WPFInpTxt1.HorizontalAlignment = "Left"
+    $WPFInpTxt1.Width = 355
+    $WPFInpTxt1.Height = 40
+    $InpTxt1_Pos = 105
+    $WPFInpTxt1.Margin = "$InpTxt1_Pos,78,0,20"
+    $WPFInpTxt1.Padding = "0,9,0,0"
+    $WPFInpTxt1.Background = "#FFFFFFFF"
+    $WPFInpTxt1.Visibility = "Visible"
+    $WPFButton1.Content.Text = "Search"
+    $WPFButton1.Content.FontSize = 20
+    $WPFButton1.Width = 130
+    $WPFButton1.Height = 40
+    $WPFButton1.HorizontalAlignment = "Left"
+    $WPFButton1.Margin = "$($InpTxt1_Pos +380),78,0,0"
+    $WPFButton1.Visibility = "Visible"
+}
+
+function Search_Layout {
+    Param ($DataSource, $Vis)
+    $WPFDataGrid1.ItemsSource = $null
+    $WPFDataGrid1.ItemsSource = $DataSource
+    $WPFDataGrid1.VerticalAlignment = "Bottom" 
+    $WPFDataGrid1.Width = $($MinWidth -90)
+    $WPFDataGrid1.Height = $($MinHeight -156)
+    $WPFDataGrid1.Margin = "0,0,0,75"
+    $WPFDataGrid1.RowHeaderWidth = 0
+    $Button_VPos2 = $($MinHeight)
+    $Button_HPos2 = $($MinWidth  -245)
+    $WPFButton2.Content.Text = "Remove"
+    $WPFButton2.Margin = "$Button_HPos2,$Button_VPos2,0,20"
+    $WPFButton3.Content.Text = "Clear"
+    $WPFButton3.Margin = "$($Button_HPos2 +105),$Button_VPos2,0,0"
+    if ($Vis -eq "Visible") {
+        $WPFDataGrid1.Visibility = "Visible"
+        $WPFButton2.Visibility = "Visible"
+        $WPFButton3.Visibility = "Visible"
+    } else {
+        $WPFDataGrid1.Visibility = "Collapse"
+        $WPFButton2.Visibility = "Collapse"
+        $WPFButton3.Visibility = "Collapse"
+    }
+}
+
+
 
 ######################################
 # Element Actions
 ######################################
 
+Default_Layout
+
 $WPFButton1.Add_Click({
+    Default_Layout
+    Search_Layout -Vis "Collapse"
+    $WPFDataGrid1.ItemsSource = $null
     if ($WPFInpTxt1.Text -match "@") {
         $UsrUPN = $WPFInpTxt1.Text
-        CallMgGraph -Meth 'Get' -Ver 'v1.0' -Res 'users' -Extra "?`$filter=userPrincipalName eq '$UsrUPN'"
+        CallMgGraph -Meth 'Get' -Ver 'v1.0' -Res 'users' -Extra "?`$filter=userPrincipalName eq '$($WPFInpTxt1.Text)'"
         $UsrID = $GrRaw.id
-        Write-Host "$($UsrID)"
-        CallMgGraph -Meth 'Get' -Ver 'v1.0' -Res "users/$UsrID/ownedDevices"
-        $OwnDev = $GrRaw #| Where-Object operatingSystem -eq 'Windows'
-        [System.Collections.ArrayList]$Script:AL = $OwnDev |
-            ForEach-Object {        
-                $_ | Select-Object `
-                    @{n="Hostname";e={$_.displayName}},`
-                    @{n="Make";e={$_.manufacturer}},`
-                    @{n="Model";e={$_.model}},`
-                    @{n="LastSync";e={$_.approximateLastSignInDateTime}},`
-                    @{n="ID";e={$_.id}}`
+        if ($null -ne $UsrID) {
+            $WPFTextB1.Text = "Users Owned Devices"
+            Get-OwnDev -UsrID $UsrID -DFilter $true
+            Search_Layout -DataSource $AL -Vis "Visible"
+        } else {
+            $WPFTextB1.Text = "UPN Not Found, Enter a Valid UPN"
+            $WPFTextB1.Foreground = "#9E2A2B"
+            $WPFInpTxt1.Background = "#E09F3E"
         }
-
-
-        #$WPFButton1.Visibility = "Hidden"
-        $WPFDataGrid1.VerticalAlignment = "Bottom" 
-        $WPFDataGrid1.Width = $($MinWidth -90)
-        $WPFDataGrid1.Height = $($MinHeight -226)
-        $WPFDataGrid1.Margin = "0,0,0,16"
-        $WPFDataGrid1.ItemsSource = $null
-        $WPFDataGrid1.ItemsSource = $AL
-        $WPFDataGrid1.RowHeaderWidth = 0
-        $WPFDataGrid1.Visibility = "Visible"
-
     }
-
-
-
-
-
 })
 
+###################### NEEDS WORK ######################
+#Remove Button Action
+$WPFButton2.Add_Click({
+    Remove-OwnDev -DevSel $WPFDataGrid1.SelectedItems -UsrID $UsrID
+    <# | ForEach-Object {
+        write-host $($_.ID)
+    }#>
+})
+
+#######################################################
+
+#Clear Button Action
+$WPFButton3.Add_Click({
+    Search_Layout -Vis "Collapse"
+    $WPFDataGrid1.ItemsSource = $null
+    $WPFInpTxt1.Text = $null
+})
 
 
 #Show GUI Window
